fe37e426fea55601f05045e7cf840c8e0a9ced16
Adarshit Soni <adarshitsoni@gmail.com>
2025-04-01 15:53:44 +0530

    Time: 603 ms (87.94%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,9 +1,5 @@
-SELECT 
-DISTINCT NUM AS ConsecutiveNums
-FROM
-(SELECT 
-NUM,
-LEAD(NUM,1) OVER(ORDER BY ID) AS NUM1,
-LEAD(NUM,2) OVER(ORDER BY ID) AS NUM2
-FROM LOGS) AS A
-WHERE (NUM=NUM1) AND (NUM=NUM2)
+/* Write your T-SQL query statement below */
+select distinct
+l1.num as ConsecutiveNums
+from logs l1,logs l2, logs l3
+where l1.id=l2.id-1 and l2.id=l3.id-1 and l1.num=l2.num and l2.num=l3.num
