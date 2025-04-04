d86511c4d5d73672e33caf515a810cbf0063367d
Adarshit Soni <adarshitsoni@gmail.com>
2025-04-04 23:49:15 +0530

    Time: 619 ms (92.26%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+select
+a.name
+from employee as a
+join (
+    select
+    managerid 
+    from employee
+    where managerid is not null
+    group by managerid
+    having count (*)>=5
+)as b on a.id=b.managerid
