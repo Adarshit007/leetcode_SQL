009d7508b8bf5b5059a72d191e7d7bd4ae279c27
Adarshit Soni <adarshitsoni@gmail.com>
2025-04-08 00:08:32 +0530

    Time: 561 ms (58.74%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,15 +1,8 @@
-/* Write your T-SQL query statement below */
-with cte as(
-    select 
-    a.product_id,
-    price,
-    units
-    from prices as a
-    left join 
-    unitssold as b
-    on a.product_id=b.product_id
-    and purchase_date >= start_date and purchase_date <= end_date
-)
-SELECT product_id, COALESCE(ROUND(CAST(SUM(price * units) AS FLOAT) / SUM(units),2),0) AS average_price
-FROM CTE
-GROUP BY product_id;
+select a.product_id,
+isnull(round(sum(a.price *b.units *1.0 )/sum(b.units),2),0) as average_price
+from prices as a
+left join 
+unitssold as b
+on a.product_id = b.product_id
+and b.purchase_date between start_date and a.end_date
+group by a.product_id
